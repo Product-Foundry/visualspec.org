fd91109880cf17ec40113bf073b30ffa6005cf08
Daniel Codrea <daniel@product-foundry.com>
2014-11-19 15:30:33 +0200

    Cover footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+// Custom scripts
+$(document).ready(function(){
+  
+  // Add current year to footer copyright
+  var currentDate = new Date();
+  $('#current-year').text((currentDate).getFullYear());
+  
+});
